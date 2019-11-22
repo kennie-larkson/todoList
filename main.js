ac5b4ff7262ac388de857f0c8efdf826cc416d70
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2019-11-22 06:41:01 +0100

    main.js refactored to use bind on document

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 
     
-    
+        var select = document.querySelector.bind(document);
+        var create = document.createElement.bind(document);
         var entries = new Array();
-        var input = document.querySelector("input");
-        var para = document.querySelector("p");
+        //var input = document.querySelector("input");
+        var input = select("input");
+        //var para = document.querySelector("p");
+        var para = select('p');
         var unOrdered = document.createElement("ul");
+        var unOrdered = create('ul');
         unOrdered.style.listStyle = 'none';
         var list;
         var deleteButton;
@@ -14,14 +18,16 @@
     function update(){
         if(input.value){
         
-            
-            list = document.createElement("li");
+            //input.value.replace(/<>/g, " ");    
+            //list = document.createElement("li");
+            list = create('li');
             list.style.backgroundColor = 'yellow';
             list.style.width = '30%';
             list.style.lineHeight = '2';
             list.style.fontSize ='20px';
             list.innerHTML = input.value;
-            deleteButton = document.createElement("button");
+            //deleteButton = document.createElement("button");
+            deleteButton = create('button');
             deleteButton.style.fontSize = '10px';
             //deleteButton.style.marginLeft = '30px';
             deleteButton.innerHTML = "Remove Item";
@@ -37,7 +43,8 @@
         
     }
 
-    p = document.createElement('p');
+    //p = document.createElement('p');
+    p = create('p');
     p.innerHTML = 'The remove item button is presently not fumctional, kindly bear with us.';
     p.style.backgroundColor = 'orange';
     p.style.width = '30%';
@@ -47,7 +54,8 @@
     
     let deleteFunc = function(){
     
-        let emptyStr = document.createTextNode(" ");
+        //let emptyStr = document.createTextNode(" ");
+        let emptyStr = create(' ')
         unOrdered.replaceChild( emptyStr , list );
     }
     
